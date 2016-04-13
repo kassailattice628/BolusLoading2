371dc729d2b037c18a4a91a3bf1e4665ef8a8181
KASAI Masatoshi <kassailattice628@gmail.com>
2016-04-13 14:22:57 +0900

    GUI，Test 機能つけた

--- BolusLoading.m
+++ BolusLoading.m
@@ -6,15 +6,6 @@ global dio
 global ui
 
 %%
-Testmode = 1;
-
-if Testmode == 0
-dio = digitalio('nidaq','dev1'); %digital object for trigger output
-addline(dio,1,1,'out');
-putvalue(dio, 0);
-end
-
-%%
 gate_time = 3;
 loop_interval = 1;
 
@@ -22,4 +13,4 @@ loop_interval = 1;
 T = timer('Period',loop_interval, 'ExecutionMode', 'fixedSpacing', 'TasksToExecute', 5);
 
 %% GUI
-ui = Open_GUI(dio, ui, gate_time, loop_interval, T, Testmode);
+ui = Open_GUI(dio, ui, gate_time, loop_interval, T);
--- Open_GUI.m
+++ Open_GUI.m
@@ -1,38 +1,58 @@
-function ui = Open_GUI(dio, ui, gate_time, loop_interval, T, Testmode)
-figure;
+function ui = Open_GUI(dio, ui, gate_time, loop_interval, T)
+Testmode = 1;
+dio = [];
 
-ui.onloop = uicontrol('style', 'togglebutton', 'string','Loop-off', 'position', [240 300 80 30],...
-    'callback', @TTLLoop, 'fontsize', 10, 'BackGroundColor', 'w');
+ui.fig = figure('Position',[10, 500, 220, 200], 'Name','BolusLoad2', 'NumberTitle', 'off', 'Menubar','none', 'Resize', 'off');
 
-ui.onpulse = uicontrol('style', 'togglebutton', 'string', 'TTLPulse', 'position', [10 300 100 30],...
-    'callback', {@TTLGateON, ui}, 'fontsize', 10);
+ui.onloop = uicontrol('style', 'togglebutton', 'string','Loop-off', 'position', [110 50 100 30],...
+    'callback', @TTLLoop, 'fontsize', 12, 'BackGroundColor', 'w');
 
-ui.ongate = uicontrol('style', 'togglebutton', 'string', 'TTLGate', 'position', [130 300 100 30],...
-    'callback', {@TTLGateON, ui}, 'fontsize', 10);
+ui.ongate = uicontrol('style', 'togglebutton', 'string', 'TTLGate', 'position', [10 50 100 30],...
+    'callback', {@TTLGateON, ui}, 'fontsize', 12);
 
-ui.off = uicontrol('string','OFF','position', [10 250 100 30],...
-    'callback', {@TTLOFF, ui}, 'fontsize', 10);
+ui.off = uicontrol('string','OFF','position', [10 10 80 30],...
+    'callback', {@TTLOFF, ui}, 'fontsize', 12);
 
-uicontrol('style','text','string','gate_time(sec)','position', [130 275 100 20], 'fontsize', 10);
+uicontrol('style','text','string','duration(s)','position', [10 115 100 20], 'fontsize', 12);
 
-ui.gatet = uicontrol('style', 'edit', 'string', gate_time, 'position', [130 250 100 25],...
+ui.gatet = uicontrol('style', 'edit', 'string', gate_time, 'position', [10 90 100 25],...
     'callback', 'gate_time = str2double(get(ui.gatet,''string''));',...
-    'fontsize', 10, 'BackGroundColor', 'w');
+    'fontsize', 12, 'BackGroundColor', 'w');
 
-uicontrol('style', 'text', 'string', 'Interval(sec)', 'position', [240 275 100 20], 'fontsize', 10);
-ui.loopint = uicontrol('style', 'edit', 'string', loop_interval, 'position', [240 250 100 25],...
+uicontrol('style', 'text', 'string', 'Interval(s)', 'position', [110 115 100 20], 'fontsize', 12);
+
+ui.loopint = uicontrol('style', 'edit', 'string', loop_interval, 'position', [110 90 100 25],...
     'callback', 'loop_interval = str2double(get(ui.loopint,''string''));',...
-    'fontsize', 10, 'BackGroundColor', 'w');
+    'fontsize', 12, 'BackGroundColor', 'w');
+
+uicontrol('style','togglebutton', 'string', 'DAQ-ON', 'position', [10, 150, 100, 30], 'callback', @setDAQ)
 
 %% set Timer FCNs %%
 T.StartFcn = @TimerStartFCN;
 T.StopFcn = {@TimerStopFCN, ui};
 T.TimerFcn = @TimerFCN;
 
+%%
+    function setDAQ(hObject, ~)
+        if get(hObject, 'value')
+            Testmode = 0;
+            % DAQ setting %Session Based DAQ
+            dev = daq.getDevices;
+            % DIO setting
+            dio = daq.createSession(dev.Vendor.ID);
+            addDigitalChannel(sTrig, dev.ID, 'port2/line4', 'OutputOnly');%Ctr0 out ‚ðŽg‚¤
+            outputSingleScan(dio,0); %reset trigger signals at Low
+        else
+            Testmode = 1;
+            putvalue(dio, 0);
+            delete dio
+            dio =[];
+        end
+    end
 
 %%
     function TTLGateON(h, ~, ui)
-        if get(h,'value')==1     
+        if get(h,'value')==1
             if get(ui.onloop, 'value')
                 set(T, 'period', loop_interval, 'ExecutionMode', 'fixedSpacing');
                 start(T)
